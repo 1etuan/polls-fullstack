5593f4802de59fc85caf1e9e5c731a78ca8b4b63
Le Tuan <lh.tuan@tego.global>
2022-09-10 23:44:07 +0700

    config jwt User Principal

--- src/main/java/com/example/polls/config/SecurityConfig.java
+++ src/main/java/com/example/polls/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.example.polls.config;
 
 import com.example.polls.security.CustomUserDetailsService;
-import com.example.polls.security.JwtAuthenticationEntryPoint;
 import com.example.polls.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
